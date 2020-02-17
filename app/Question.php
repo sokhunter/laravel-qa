b90c83c32e8a0a7bead6c8cf080fd38bc4f96712
sokhunter <emylivaque@gmail.com>
2020-02-17 16:35:56 -0500

    Prepupulate users and questions tables using model factories and seed

--- app/Question.php
+++ app/Question.php
@@ -3,6 +3,7 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\support\Str;
 
 class Question extends Model
 {
@@ -10,6 +11,12 @@ class Question extends Model
     
     public function user()
     {
-        return $this->belongTo(User::class)
+        return $this->belongsTo(User::class);
+    }
+
+    public function setTitleAttribute($value)
+    {
+        $this->attributes['title'] = $value;
+        $this->attributes['slug'] = Str::slug($value);
     }
 }
